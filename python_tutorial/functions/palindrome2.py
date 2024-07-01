8a54f2e850f86c3d9ff9a5a2fc36cf4b6b7304be
Akhil <96526208+Akhil-Tesla@users.noreply.github.com>
2024-07-01 22:34:55 +0530

    Update palindrome2.py

--- python_tutorial/functions/palindrome2.py
+++ python_tutorial/functions/palindrome2.py
@@ -1,10 +1,11 @@
 #!/bin/python3
-def palindrome(strings,string2):
-    n1=sorted(strings)
-    n2=sorted(string2)
-    if n1 == n2:
-        return True
-    else:
-        return False
-        
+strings=input("ENter a string: ")
+string2= input("Enter another string: ")
+n1=sorted(strings)
+n2=sorted(string2)
+if n1 != n2:
+    print("False")
+else:
+    print("True")
+
   
