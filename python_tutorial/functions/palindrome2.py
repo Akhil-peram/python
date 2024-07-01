8881616b1fe4f7da7e77a82118c3a0f59f489275
Akhil <96526208+Akhil-Tesla@users.noreply.github.com>
2024-07-01 22:20:52 +0530

    Create palindrome2.py

---
+++ python_tutorial/functions/palindrome2.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+def palindrome(strings,string2):
+    n1=sorted(strings)
+    n2=sorted(string2)
+    if n1 == n2:
+        return true
+    else:
+        return false
+        
+  
