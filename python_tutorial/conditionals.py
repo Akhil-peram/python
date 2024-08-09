71ab99f6b3582ceb1ce95f487e686c7316d673da
Akhil <96526208+Akhil-Tesla@users.noreply.github.com>
2024-08-09 10:01:02 +0530

    Update conditionals.py

--- python_tutorial/conditionals.py
+++ python_tutorial/conditionals.py
@@ -1,1 +1,11 @@
+# conditional statements are divided into if else , if elif else , match(like switch cases) 
+
+age =18
+if age>18:
+  print("you can drive a Car")
+else:
+  print("you can't drive")
+
+print("This statement is always printed") # This statement is outside of conditional statement if else(Dont have to write this
+
 
