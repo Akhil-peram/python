44d9fbeba91c3ac31e2bbc2c5bc3d0701cfc8187
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2025-12-12 14:18:04 +0530

    Implement Calculator class with basic operations in OOP

--- python_tutorial/calculator.py
+++ python_tutorial/calculator.py
@@ -1,1 +1,24 @@
+# calculator in OOP
+class Calculator:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return f"{self.x} + {self.y} = {self.x + self.y}"
+
+    def sub(self):
+        return f"{self.x} - {self.y} = {self.x - self.y}"
+
+    def div(self):
+        return f"{self.x} / {self.y} = {self.x / self.y}"
+
+    def multiple(self):
+        return f"{self.x} * {self.y} = {self.x * self.y}"
+
+
+cal = Calculator(9, 5)
+
+print(cal.add())
+print(cal.multiple())
 
